bd7eda20a8d695cfca1a594414a0aa140f770041
Rashi1997 <rdhar1997@gmail.com>
2021-02-17 22:25:35 -0500

    feat: fix workflow

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  transpileDependencies: ["vuetify"],
+  pluginOptions: {
+    i18n: {
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: false
+    },
+    publicPath:
+      process.env.NODE_ENV === "production" ? "/madlib/" : "/"
+  },
+  chainWebpack: config => {
+    const svgRule = config.module.rule("svg");
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use("babel-loader")
+      .loader("babel-loader")
+      .end()
+      .use("vue-svg-loader")
+      .loader("vue-svg-loader");
+  }
+};
